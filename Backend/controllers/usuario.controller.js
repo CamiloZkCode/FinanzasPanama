0fd5d887bd01b37d7a8737e841521251b2d9d760
Camilo Pimiento <camilopimientogarcia09@gmail.com>
2025-08-04 18:38:52 -0500

    usuarios tablas

--- Backend/controllers/usuario.controller.js
+++ Backend/controllers/usuario.controller.js
@@ -11,7 +11,20 @@ const getSupervisores = async (req, res) => {
   }
 };
 
+const getUsuariosxAdmin = async (req, res) => {
+  try {
+    const supervisores = await usuariosModel.obtenerSupervisores();
+    res.status(200).json(supervisores);
+  } catch (error) {
+    console.error('Error al obtener supervisores:', error);
+    res.status(500).json({ mensaje: 'Error del servidor al obtener supervisores' });
+  }
+};
+
+
+
+
 module.exports = {
- 
+  getUsuariosxAdmin,
   getSupervisores
   };
--- Backend/models/user.models.js
+++ Backend/models/user.models.js
@@ -1,22 +1,52 @@
-const db = require('../config/db');
+const db = require("../config/db");
 
 const getAllUsuarios = (callback) => {
-  db.query('SELECT * FROM usuarios', callback);
+  db.query("SELECT * FROM usuarios", callback);
 };
 
 async function findUserByUsername(username) {
-  const [rows] = await db.query('SELECT * FROM usuarios WHERE username = ?', [username]);
+  const [rows] = await db.query("SELECT * FROM usuarios WHERE username = ?", [
+    username,
+  ]);
   return rows[0];
 }
 
 async function getRolById(id_rol) {
-  const [rows] = await db.query('SELECT rol FROM roles WHERE id_rol = ?', [id_rol]);
+  const [rows] = await db.query("SELECT rol FROM roles WHERE id_rol = ?", [
+    id_rol,
+  ]);
   return rows[0]?.rol;
 }
 
-
 const obtenerSupervisores = async () => {
-  const [rows] = await db.query('SELECT id_usuario AS id, nombre FROM usuarios WHERE id_rol = 2');
+  const [rows] = await db.query(
+    "SELECT id_usuario AS id, nombre FROM usuarios WHERE id_rol = 2"
+  );
+  return rows;
+};
+
+const obtenerUsuariosxAdmin = async (id_admin) => {
+  const [rows] = await db.query(
+    `
+    SELECT    
+      u1.id_usuario AS id,
+      u1.nombre,
+      u1.telefono,
+      u1.correo,
+      u1.username,
+      u1.id_rol,
+      u1.id_administrador,
+      u1.id_supervisor
+    FROM usuarios u1
+    LEFT JOIN usuarios u2 ON u1.id_supervisor = u2.id_usuario
+    WHERE    
+      (u1.id_rol = 2 AND u1.id_administrador = ?)
+      OR 
+      (u1.id_rol = 3 AND u2.id_administrador = ?)
+  `,
+    [id_admin, id_admin]
+  );
+
   return rows;
 };
 
@@ -24,5 +54,6 @@ module.exports = {
   getAllUsuarios,
   findUserByUsername,
   getRolById,
-  obtenerSupervisores
+  obtenerSupervisores,
+  obtenerUsuariosxAdmin,
 };
